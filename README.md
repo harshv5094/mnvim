7877604196445c4c08a1e0b7bfd4afbb1b93f769
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2025-08-01 19:17:42 +0530

    feat: add setup script

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # mnvim (my minimal nvim)
 
 this is a minimal nvim config.
+
+**Just Run This Script.** Make Sure nvim is installed
+
+```sh
+curl -fsLo /tmp/setup.sh https://raw.githubusercontent.com/harshv5094/mnvim/refs/heads/main/setup.sh && sh /tmp/setup.sh && rm -rf /tmp/setup.sh
+
+```
---
+++ setup.sh
@@ -0,0 +1,64 @@
+#!/bin/sh -e
+
+error_msg() {
+  printf "\033[1;31m%b\033[0m\n" "$1"
+}
+
+# Check for sudo execution
+if [ "$(id -u)" -eq 0 ]; then
+  error_msg "Should not be ran as root !"
+  exit 1
+fi
+
+warning_msg() {
+  printf "\033[0;33m%b\033[0m\n" "$1"
+}
+
+info_msg() {
+  printf "\033[0;36m%b\033[0m\n" "$1"
+}
+
+success_msg() {
+  printf "\033[0;32m%b\033[0m\n" "$1"
+}
+
+# Command Existence Checker #
+have() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+if have nvim; then
+  success_msg "Neovim Exist"
+else
+  warning_msg "Please install nvim then run script again"
+  exit 1
+fi
+
+# Setup Remotes #
+setupRemotes() {
+  warning_msg "Setting Up Remotes"
+  git remote | grep -q "^origin$" && git remote rm origin
+  git remote | grep -q "^github$" || git remote add github git@github.com:harshv5094/dotfiles.git
+  git remote | grep -q "^gitlab$" || git remote add gitlab git@gitlab.com:harshv5094/dotfiles.git
+  git branch --set-upstream-to=github/main main
+}
+
+# Global Variables #
+CONFIG_DIR="$HOME/.config"
+NEOVIM_DIR="$HOME/.config/nvim"
+
+if [ ! -d "${CONFIG_DIR}" ]; then
+  mkdir -p "${CONFIG_DIR}"
+fi
+
+if [ ! -d "${NEOVIM_DIR}" ]; then
+  info_msg "Cloning my dotfiles repository..."
+  git clone https://github.com/harshv5094/mnvim "${NEOVIM_DIR}"
+  setupRemotes
+  cd "${NEOVIM_DIR}" || return
+  success_msg "NEOVIM_DIR: $(pwd)"
+else
+  cd "${NEOVIM_DIR}" || return
+  setupRemotes
+  success_msg "NEOVIM_DIR: $(pwd)"
+fi
